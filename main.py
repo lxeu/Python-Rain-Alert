dc01c1f4dacc771b4e43130f39639acfa9ca56bf
lxeu <wangmatthew6000@gmail.com>
2026-04-01 13:39:31 -0600

    Use environment variables for API keys
    Replace hardcoded API key with environment variable.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 import os
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "edfef592cf27f80eda19d3d2adb6c262"
+api_key = os.environ.get("OWM_API_KEY")
 account_sid = os.environ.get("ACCOUNT_SID")
 auth_token = os.environ.get("AUTH_TOKEN")
 
